0cd507d5ecf5ed281ba61ddb71ea2492278b3184
j-n4m4573 <jmg220p@gmail.com>
2016-01-18 13:33:57 -0800

    Initial Commit

---
+++ Deprocrastinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Deprocrastinator
+//
+//  Created by Jamar Gibbs on 1/18/16.
+//  Copyright © 2016 M1ndful M3d1a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
